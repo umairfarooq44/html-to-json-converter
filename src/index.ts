571d6a92a93e051e591930cd4de3d833d9f6e2b7
Umair Farooq <umairfarooq44@gmail.com>
2023-09-11 16:15:14 +0500

    Released new package

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import fs from 'fs';
+import { isNode } from 'browser-or-node';
 import htmlToJson from './htmlToJson';
 
-if (process.argv[2]) {
-  fs.readFile(process.argv[2], 'utf8', (err, file) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.stringify(htmlToJson(file), null, 2));
-    }
-  });
+if (isNode) {
+  const fs = require('fs');
+  if (process.argv[2]) {
+    fs.readFile(
+      process.argv[2],
+      'utf8',
+      (err: NodeJS.ErrnoException | null, file: string) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.stringify(htmlToJson(file), null, 2));
+        }
+      }
+    );
+  }
 }
 
 export { htmlToJson };
